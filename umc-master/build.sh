49aed6dda102c16f7d62da9fce3f3414d387cf23
Minji Kim <minjikim2000@hotmail.com>
2025-01-06 05:06:22 +0900

    🔧 config: build.sh 수정

--- umc-master/build.sh
+++ umc-master/build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-cd ../
+# 현재 디렉토리를 umc-master로 이동
+cd umc-master
 
+# output 디렉토리 생성 (이미 존재하면 삭제 후 생성)
+rm -rf output
 mkdir output
 
-cp -R ./frontend/* ./output
+# umc-master 내부의 모든 파일을 output 디렉토리로 복사
+cp -R ./* ./output
 
-cp -R ./output ./frontend/
+# 필요한 경우 output 디렉토리에서 추가 작업 수행 가능
